78abbf4de06879a443c611e196fc98497d3b4713
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2018-06-11 20:42:01 +0200

    Update

--- components/App.js
+++ components/App.js
@@ -30,7 +30,7 @@ var App = React.createClass({
     return (
         <div className={'app'}>
             <ContactForm contac={contactForm}/>
-            <ContactsForm items={contacts}/>
+            <Contacts items={contacts}/>
         </div>
     );
   }
